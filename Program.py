b8bea20b68223954dd87b603244145379d7a32f2
JokNavi <JokNavi1@gmail.com>
2022-10-03 15:38:36 +0200

    End of session

--- Program.py
+++ Program.py
@@ -1,75 +1,64 @@
 from numpy import random
 #Normal difficulty
 #  Operators:
-OPERATORS = ["*","/","+","-"]
+OPERATORS = ['* or /', '+ or -']
+OPERATORS_CHANCES = [0.5, 0.5]
+AMOUNT_OF_OPERATIONS = [5, 8, 10, 13, 15]
+AMOUNT_OF_OPERATIONS_CHANCES = [0.15, 0.2, 0.3, 0.2, 0.15]
+AMOUNT_OF_OPERATIONS = random.choice(AMOUNT_OF_OPERATIONS, p=AMOUNT_OF_OPERATIONS_CHANCES, size=(1)).tolist()
+USER_OPERATIONS = random.choice(OPERATORS, p=OPERATORS_CHANCES, size=(AMOUNT_OF_OPERATIONS)).tolist()
+#  Game settings:
+BURN_HEIGHT = 250
+GAME_MODE = 'Normal'
+ACCEPTED_NUMBERS = [str(i) for i in range(10) if i != 0]
+# INIT:
+high_score = 0
 
-with open('Settings.txt', 'r') as s:
-    settings_file = s.readlines()
-operator_chances = settings_file[settings_file.index("    Operators:\n")+1:settings_file.index("    Accepted numbers:\n")-1]
-accepted_numbers = settings_file[settings_file.index("    Accepted numbers:\n")+1:settings_file.index("    Amount of operations:\n")-1]
-game_mode = settings_file[settings_file.index("  Game mode:\n")+1:settings_file.index("Guide:\n")-1]
-game_mode = str(*game_mode).lstrip("    ").rstrip("\n")
-amount_of_operations = settings_file[settings_file.index("    Amount of operations:\n")+1:settings_file.index("    Burn height:\n")-1]
-amount_of_operations = [str(operation).lstrip("      ").rstrip("\n")for operation in amount_of_operations]
-burn_height = settings_file[settings_file.index("    Burn height:\n")+1:settings_file.index("  Master level:\n")-1]
-BURN_HEIGHT = [str(height).lstrip("      ").rstrip("\n")for height in burn_height][0]
-AMOUNT_OF_OPERATIONS = random.choice([value[0:2] for value in amount_of_operations], p=[value[3:8] for value in amount_of_operations], size=(1)).tolist()
-OPERATIONS = random.choice(OPERATORS, p=[value[13:18] for value in operator_chances], size=(int(AMOUNT_OF_OPERATIONS[0]))).tolist()
-
-accepted_numbers = [str(number).lstrip("    ").rstrip("\n")for number in accepted_numbers]
 while True:
-    print(f"Input your number! [{accepted_numbers[0]}-{accepted_numbers[-1]}]")
+    print(f"Input your number! [{ACCEPTED_NUMBERS[0]}-{ACCEPTED_NUMBERS[-1]}]")
     number = input("->: ")
-    if number in accepted_numbers:
+    if number in ACCEPTED_NUMBERS:
         number = int(number)
         break
     print(f"Sorry. {number} is not an allowed number!\n")
-while len(OPERATIONS) > 0:
+    
+while len(USER_OPERATIONS) > 0:
     print(f"\nYour number: {number}")
     print(f"Burn height: {BURN_HEIGHT}")
     print(f"Your high score: {high_score}")
     print("Pick an operator: ")
-    print(f"{OPERATORS[0]} or {OPERATORS[1]}", end="")
-    print(f"  [{ OPERATIONS.count(str(operator_chances[0][6:12]))}]")
-    print(f"{OPERATORS[2]} or {OPERATORS[3]}", end="")
-    print(f"  [{OPERATIONS.count(str(operator_chances[1][6:12]))}]", end="\n\n")
+    print(f"{OPERATORS[0]} [{USER_OPERATIONS.count(OPERATORS[0])}x]")
+    print(f"{OPERATORS[1]} [{USER_OPERATIONS.count(OPERATORS[1])}x]")
     chosen_operator = input("->: ")
-    if chosen_operator in OPERATORS:
+    if [v for _,v in enumerate(USER_OPERATIONS) if chosen_operator in v]:
         match chosen_operator:
-            case "^":
-                if game_mode == "Numbered":
-                    print(f"->:{[number][0]}^{[number][0]}")
-                    number = int([number][0]**[number][0])
             case "*":
-                print(f"->:{number}*{[number][0]}")
+                print(f"{number}*{[number][0]}")
                 number = int(number * [number][0])
             case "/":
-                print(f"->:{number}/{[number][0]}")
+                print(f"{number}/{[number][0]}")
                 number = int(number / [number][0])
             case "+":
-                print(f"->:{number}+{sum([number])}")
+                print(f"{number}+{sum([number])}")
                 number = int(number + sum([number]))
             case "-":
-                print(f"->:{number}-{sum([number])}")
+                print(f"{number}-{sum([number])}")
                 number = int(number - sum([number]))
-        if number == 0:
-            number = -5
-        elif number > int(BURN_HEIGHT):
+        if (number == 0): number = 5
+        elif (number > high_score): high_score = number
+        if (number > BURN_HEIGHT): 
             print("We're sorry you lost!")
-            print(f"Your number: {number} > Burn number: {BURN_HEIGHT}")
+            print(f"Your number: {number}, is larger then burn number: {BURN_HEIGHT}")
             break
-        if chosen_operator in list(str([item for item in OPERATIONS])):
-            f = [value for value in OPERATIONS if chosen_operator in value]
-            OPERATIONS.remove(f[0])
+        operator_quantity_check = str([v for _,v in enumerate(USER_OPERATIONS) if chosen_operator in v][0])
+        if(operator_quantity_check): #remove used operation
+            USER_OPERATIONS.remove(operator_quantity_check)
         else:
             print(f"Nope, no more \"{chosen_operator}\" left over. sorry!")
-        if number > high_score:
-            high_score = number
-        if len(OPERATIONS) <= 0:
-            if number <= 0:
-                print(f"You win! Your high score was: {high_score}")
-            else:
-                print(f"Your number: {number}")
-                print("We're sorry you lost!")
+        if number <= 0 and len(USER_OPERATIONS) <= 0: #win condition
+            print(f"You win! Your high score was: {high_score}") 
+        elif len(USER_OPERATIONS) <= 0:
+            print(f"Your number: {number}")
+            print("We're sorry you lost!")
     else:
         print("Invalid operator. Please choose another one.")
